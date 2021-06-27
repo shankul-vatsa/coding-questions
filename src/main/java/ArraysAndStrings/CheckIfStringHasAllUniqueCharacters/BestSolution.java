8b1ca1555d7fb71c8604b21577325adbe1feff32
Shankul Vatsa <shankul.angel@gmail.com>
2021-06-27 18:28:50 +0530

    Solution for - 1.1 Find if the given string has all the unique characters (Page-60)

--- src/main/java/ArraysAndStrings/CheckIfStringHasAllUniqueCharacters/BestSolution.java
+++ src/main/java/ArraysAndStrings/CheckIfStringHasAllUniqueCharacters/BestSolution.java
@@ -6,8 +6,10 @@ import org.apache.commons.lang3.StringUtils;
  * @author Shankul Vatsa
  * on 27-06-2021
  */
-//This improves on the space complexity of the NearToBestSolution.
-//Space complexity reduces to exactly constant time O(1), time complexity same as O(N).
+//O(N) implementation, can be even regarded as O(1) as the loop
+//goes through at most 128/256 iterations.
+
+//Takes almost constant space (boolean[128] or boolean[256])
 public class BestSolution {
     public static void main(String[] args) {
         final String testStr = "qwertqyuio";
@@ -18,16 +20,23 @@ public class BestSolution {
             System.out.println(testStr + " is not an unique string");
         }
     }
-
     private static boolean checkIfUnique(final String testStr) {
         if(StringUtils.isEmpty(testStr)) {
             return true;
         }
 
-        for(int i = 0; i < testStr.length()-1; ++i) {
+        //assuming only ASCII characters, can be 256 also for extended ASCII or Unicode
+        final boolean[] dupTestArr = new boolean[128];
 
+        for(int i = 0; i < testStr.length(); ++i) {
+            char ithChar = testStr.charAt(i);
+            if (dupTestArr[ithChar]) {
+                return false;
+            }
+            dupTestArr[ithChar] = true;
         }
 
         return true;
     }
+
 }
